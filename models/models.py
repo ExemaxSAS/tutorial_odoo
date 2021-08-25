6a284e16229a5bb5afe6b285297a772e50a459e0
Ignacio Buioli <ibuioli@gmail.com>
2021-08-25 18:07:09 -0300

    feat: add fields

--- models/models.py
+++ models/models.py
@@ -5,5 +5,9 @@ from odoo import models, fields, api
 class TutorialOdoo(models.Model):
     _inherit = "sale.order"
 
-    test_bool = fields.Boolean('Test')
+    test_bool = fields.Boolean('Test Bool')
+    test_int = fields.Integer('Test Int')
+    test_float = fields.Float('Test Float')
     test_char = fields.Char('Test Char')
+    test_text = fields.Text('Test Text')
+    test_selection = fields.Selection([('type1', 'Tipo 1'),('type2', 'Tipo 2')], 'Test Selection')
