a37f28dfd4565787496f18ac07f3ddeeb8626f91
Ignacio Buioli <ibuioli@gmail.com>
2021-08-26 19:36:59 -0300

    feat: add complex fields

--- models/models.py
+++ models/models.py
@@ -11,3 +11,18 @@ class TutorialOdoo(models.Model):
     test_char = fields.Char('Test Char')
     test_text = fields.Text('Test Text')
     test_selection = fields.Selection([('type1', 'Tipo 1'),('type2', 'Tipo 2')], 'Test Selection')
+
+    test_date = fields.Date('Test Date')
+    test_html = fields.Html('Test HTML')
+    test_binary = fields.Binary('Test Binary')
+
+    test_many2one = fields.Many2one('res.partner', 'Test Many2one')
+    test_one2many = fields.One2many(comodel_name='sale.order.tutorial', inverse_name='sale_order_id', string="Test One2Many")
+
+class TutorialOdooLine(models.Model):
+    _name = "sale.order.tutorial"
+
+    name = fields.Char('Nombre')
+    price = fields.Float('Precio')
+    # Campos de Control
+    sale_order_id = fields.Many2one('sale.order', 'Sale ID')
